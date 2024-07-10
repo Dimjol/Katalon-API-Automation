f281d0c60a199706d37c8615529bc8bffb2d0f14
DIMAS MAHESA <96423520+Dimjol@users.noreply.github.com>
2024-07-10 08:33:41 +0700

    Automation API Katalon

--- Scripts/Test Users/Test List Users/Script1720325297466.groovy
+++ Scripts/Test Users/Test List Users/Script1720325297466.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Response = WS.sendRequest(findTestObject('Test List Users'))
+Response = WS.sendRequest(findTestObject('Test Users/Test List Users'))
 
 WS.verifyResponseStatusCode(Response, 200)
 
---
+++ Test Suites/Smoke Test Users.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Test Users</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>50c460f7-dbed-493b-bd53-ea7e98a057b3</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c9eff6c-108f-4cd5-87c5-6acb81584ca2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Users/Test List Users</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1863db6c-2eb2-41fa-90bb-491611cd911a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Users/Test Single Users</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
